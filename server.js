47f0ed64461473e1eff384e75ecd58f9d952e33f
Lane275 <mhy2596752@outlook.com>
2025-08-29 16:36:34 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // 中间件
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(express.static(path.join(__dirname)));
+ 'public')));
 
 // 模拟数据库
 let orders = [];
